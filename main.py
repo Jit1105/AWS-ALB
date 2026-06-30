f146498ba1be0a1ad9e5bc6c4186389b64faef6d
Jit <pateljit360@gmail.com>
2026-06-30 12:07:19 +0530

    Adding new code for health

--- main.py
+++ main.py
@@ -9,5 +9,12 @@ def greet():
         "message": "I am Jit Patel, Hello Everyone"
     }
 
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8081)
