936f8b6e635c216352d08cf0d894d85d6b79c8dd
Jit <pateljit360@gmail.com>
2026-06-30 22:31:53 +0530

    Adding script file for automation of setup

---
+++ script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "updating system"
+sudo apt update
+
+echo "checking version of git"
+git --version
+
+echo "download git"
+sudo apt install git -y
+
+echo "cloning the repos"
+git clone https://github.com/Jit1105/AWS-ALB.git
+
+echo "downloading python"
+sudo apt install python3
+
+echo "download fastapi"
+sudo apt install python3-fastapi
+
+echo "running fastApi application"
+python3 /home/ubuntu/AWS-ALB/main.py
+
+echo "done"
